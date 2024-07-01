71bc13fb9df0b671ca6a7a1635d15595ee4720c5
maitulya7 <vaghelamaitulya85@gmail.com>
2024-07-01 20:46:15 +0530

    TabBar

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,15 @@
 import { Text, View } from 'react-native'
 import React from 'react'
-import { Link } from 'expo-router'
+import { Link} from 'expo-router'
 
 const index = () => {
+
+
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Maitulya</Text>
-      <Link href="/profile" style={{color:"blue"}}>Go to Profile</Link>
+      <Text className="text-3xl font-pbold">Welcome! Maitulya</Text>
+      <Link href="/home" style={{color:"blue"}}>Go to Home</Link>
     </View>
   )
 }
