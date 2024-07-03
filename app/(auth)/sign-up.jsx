6d4a8822fd3e21549f16d56c9bb2e9b65b44b329
maitulya7 <vaghelamaitulya85@gmail.com>
2024-07-03 16:18:42 +0530

    Auth

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,65 +1,95 @@
-import { View, Text, ScrollView, Image } from "react-native";
-import React, { useState } from "react";
+import { useState } from "react";
+import { Link, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
+
 import { images } from "../../constants";
+import { createUser } from "../../lib/appwrite";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+
 const SignUp = () => {
+  const [isSubmitting, setSubmitting] = useState(false);
   const [form, setForm] = useState({
-    username:"",
+    username: "",
     email: "",
     password: "",
   });
-  const [isSubmitting, setisSubmitting] = useState(false);
 
-  const sumbit = () => {};
+  const submit = async () => {
+    if (form.username === "" || form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+
+    setSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+        <View
+          className="w-full flex justify-center h-full px-4 my-6"
+          style={{
+            minHeight: Dimensions.get("window").height - 100,
+          }}
+        >
           <Image
-            resizeMode="contain"
             source={images.logo}
-            className="w-[115px] h-[35px]"
+            resizeMode="contain"
+            className="w-[115px] h-[34px]"
           />
-          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
-            Sing up in to Aora
+
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Sign Up to Aora
           </Text>
+
           <FormField
             title="Username"
             value={form.username}
-            handleChnageText={(e) => setForm({ ...form, username: e })}
-            otherStyle="mt-10"
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
           />
+
           <FormField
             title="Email"
             value={form.email}
-            handleChnageText={(e) => setForm({ ...form, email: e })}
-            otherStyle="mt-7"
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
             keyboardType="email-address"
           />
+
           <FormField
             title="Password"
             value={form.password}
-            handleChnageText={(e) => setForm({ ...form, password: e })}
-            otherStyle="mt-7"
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
           />
+
           <CustomButton
             title="Sign Up"
+            handlePress={submit}
             containerStyles="mt-7"
-            handlePress={sumbit}
             isLoading={isSubmitting}
           />
-          <View className="justify-center pt-5 flex-row gap-2">
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
             <Text className="text-lg text-gray-100 font-pregular">
-              Already have an account?
+              Have an account already?
             </Text>
             <Link
               href="/sign-in"
-              className="text-lg text-secondary font-psemibold"
+              className="text-lg font-psemibold text-secondary"
             >
-              Sign In
+              Login
             </Link>
           </View>
         </View>
