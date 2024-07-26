bc3d6f4eab71437d5634296f8c433dc2c003272a
maitulya7 <vaghelamaitulya85@gmail.com>
2024-07-26 12:13:08 +0530

    Search

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,41 +1,45 @@
 import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
 
 import { icons } from "../constants";
+import { router, usePathname } from "expo-router";
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
-  const [showPassword, setShowPassword] = useState(false);
+const SearchInput = ({initalQuery}) => {
+  const pathName = usePathname();
+  const [query, setQuery] = useState(initalQuery || "");
 
   return (
-
-
     <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
-        <TextInput
-          className="flex-1 text-white font-psemibold text-base"
-          value={value}
-          placeholder="Seach for video topic"
-          placeholderTextColor="#7B7B8B"
-          onChangeText={handleChangeText}
-          secureTextEntry={title === "Password" && !showPassword}
-          {...props}
-        />
+      <TextInput
+        className="flex-1 text-white font-psemibold text-base"
+        value={query}
+        placeholder="Seach for video topic"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+      />
 
-        <TouchableOpacity>
-            <Image
-                source={icons.search}
-                className="w-5 h-5"
-                resizeMode="contain"
-
-            />
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert(
+              "Missing Query",
+              "Please enter a query to search"
+            );
+          }
+          if (pathName.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
+      </TouchableOpacity>
+    </View>
   );
 };
 
