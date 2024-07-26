a97c89208388558860c7a088d63c218532af2e3e
maitulya7 <vaghelamaitulya85@gmail.com>
2024-07-26 12:46:44 +0530

    Profile Screen

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,12 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "../../components/EmptyState";
+import { getUserPost } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { icons } from "../../constants";
+import InfoBox from "../../components/InfoBox";
 
 const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+  const { user, setUser, isLogged } = useGlobalContext();
+  const { data: Posts } = useAppwrite(() => getUserPost(user.$id));
 
-export default Profile
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={Posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
+
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image
+                source={{uri: user?.avatar}}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+            <InfoBox/>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No videos found"
+            subtitle="No videos found for this search query!"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
